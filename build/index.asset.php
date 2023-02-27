cdaad49e4ec01031eed8f0296489f84a3aa62263
sakhawat9 <sakhawathossain7969@gmail.com>
2023-02-27 13:39:02 +0600

    uppdate

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c25c5a7ad0eab8ad368b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '265e133d48fe0c4602fa');
