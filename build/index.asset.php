17aaea7d5749fe08df28013d9e19c728115547c2
sakhawat9 <sakhawathossain7969@gmail.com>
2023-02-28 13:25:32 +0600

    updat social icon

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9fb36ac6994f8546c0de');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2de1c3306e175839d0f9');
